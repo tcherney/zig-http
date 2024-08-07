48a136473cdb5f8f0af7bfa38d11b0d17a1624db
Timothy Cherney <tjcherney@hotmail.com>
2024-08-07 00:55:30 -0700

    started work on httpserver struct, can use the tcp socket and a thread pool to communicate with many clients

---
+++ src/http_server.zig
@@ -0,0 +1,58 @@
+const std = @import("std");
+const tcp_socket = @import("tcp_socket.zig");
+
+const NUM_THREADS: comptime_int = 50;
+
+pub const HttpServer = struct {
+    socket: tcp_socket.TCPSocket = undefined,
+    thread_pool: *std.Thread.Pool = undefined,
+    is_running: bool = true,
+    allocator: std.mem.Allocator = undefined,
+
+    pub fn init(address: []const u8, port: u16, allocator: std.mem.Allocator) !HttpServer {
+        var thread_pool: *std.Thread.Pool = try allocator.create(std.Thread.Pool);
+        try thread_pool.init(.{ .allocator = allocator, .n_jobs = NUM_THREADS });
+        return HttpServer{ .socket = try tcp_socket.TCPSocket.init(address, port), .thread_pool = thread_pool, .allocator = allocator };
+    }
+
+    pub fn handle_client(self: *HttpServer, client: *tcp_socket.TCPSocket) void {
+        defer self.deinit_client(client);
+        var buffer: [1024]u8 = undefined;
+        const bytes = client.receive(&buffer) catch {
+            std.debug.print("Error recieving data\n", .{});
+            return;
+        };
+        std.debug.print("received {d} bytes: {s}\n", .{ bytes, buffer });
+    }
+
+    pub fn deinit_client(self: *HttpServer, client: *tcp_socket.TCPSocket) void {
+        client.close();
+        self.allocator.destroy(client);
+    }
+
+    pub fn deinit(self: *HttpServer) !void {
+        self.is_running = false;
+        self.thread_pool.deinit();
+        self.socket.close();
+        self.allocator.destroy(self.thread_pool);
+    }
+
+    pub fn start(self: *HttpServer) !void {
+        try self.socket.bind();
+        try self.socket.listen();
+        while (self.is_running) {
+            const client: *tcp_socket.TCPSocket = try self.allocator.create(tcp_socket.TCPSocket);
+            client.* = try self.socket.accept();
+            try self.thread_pool.spawn(handle_client, .{ self, client });
+        }
+        self.socket.close();
+    }
+};
+
+test "echo" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var server = try HttpServer.init("127.0.0.1", 8888, allocator);
+    std.debug.print("{}", .{server});
+    try server.start();
+}
--- src/tcp_socket.zig
+++ src/tcp_socket.zig
@@ -1,49 +1,57 @@
+//https://github.com/ikskuh/zig-network/blob/master/network.zig
 const std = @import("std");
 
-pub const Socket = struct {
+pub const TCPSocket = struct {
     address: std.net.Address,
     socket: std.posix.socket_t,
 
-    pub fn init(ip: []const u8, port: u16) !Socket {
+    pub fn init(ip: []const u8, port: u16) !TCPSocket {
         const parsed_address = try std.net.Address.parseIp4(ip, port);
         const sock: std.posix.socket_t = try std.posix.socket(std.posix.AF.INET, std.posix.SOCK.STREAM, 0);
         errdefer std.posix.close(sock);
-        return Socket{ .address = parsed_address, .socket = sock };
+        return TCPSocket{ .address = parsed_address, .socket = sock };
     }
 
-    pub fn bind(self: *const Socket) !void {
+    pub fn bind(self: *const TCPSocket) !void {
         try std.posix.bind(self.socket, &self.address.any, self.address.getOsSockLen());
     }
 
-    pub fn accept(self: *const Socket) !Socket {
+    pub fn accept(self: *const TCPSocket) !TCPSocket {
         var addr: std.net.Address = undefined;
         var addr_size: std.posix.socklen_t = @sizeOf(std.net.Address);
         const add_ptr: *std.posix.sockaddr = @ptrCast(&addr);
         const client = try std.posix.accept(self.socket, add_ptr, &addr_size, 0);
         errdefer std.posix.close(client);
-        return Socket{
+        return TCPSocket{
             .address = addr,
             .socket = client,
         };
     }
-    pub fn listen(self: *const Socket) !void {
+    pub fn listen(self: *const TCPSocket) !void {
         try std.posix.listen(self.socket, 0);
     }
 
-    pub fn receive(self: *const Socket, data: []u8) !usize {
+    pub fn receive(self: *const TCPSocket, data: []u8) !usize {
         return try std.posix.recvfrom(self.socket, data, 0, null, null);
+    }
+
+    pub fn close(self: *const TCPSocket) void {
+        std.posix.close(self.socket);
     }
 };
 
 test "echo" {
-    const server = try Socket.init("127.0.0.1", 8888);
+    const server = try TCPSocket.init("127.0.0.1", 8888);
     std.debug.print("{}", .{server});
     try server.bind();
     try server.listen();
     var buffer: [1024]u8 = undefined;
     while (true) {
-        const client: Socket = try server.accept();
+        const client: TCPSocket = try server.accept();
         const bytes = try client.receive(&buffer);
         std.debug.print("received {d} bytes: {s}\n", .{ bytes, buffer });
+        client.close();
+        break;
     }
+    server.close();
 }
