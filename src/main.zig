4053403c820b39885af5a3993cc99863a048e765
Timothy Cherney <tjcherney@hotmail.com>
2024-08-09 23:22:14 -0700

    small cmd line interface

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,32 @@
 //https://datatracker.ietf.org/doc/html/rfc9112
-//https://bhch.github.io/posts/2017/11/writing-an-http-server-from-scratch/
 const std = @import("std");
+const http_server = @import("http_server.zig");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    const argsv = try std.process.argsAlloc(allocator);
+    var server: http_server.HttpServer = undefined;
+    if (argsv.len > 1) {
+        if (argsv.len < 3) {
+            try stdout.print("Usage: {s} ip4_address port\n", .{argsv[0]});
+            try bw.flush();
+            return;
+        }
+        const port = std.fmt.parseInt(u16, argsv[2], 10) catch {
+            try stdout.print("Usage: {s} ip4_address port\n", .{argsv[0]});
+            try bw.flush();
+            return;
+        };
+        server = try http_server.HttpServer.init(argsv[1], port, allocator);
+    } else {
+        server = try http_server.HttpServer.init("127.0.0.1", 8888, allocator);
+    }
+    std.process.argsFree(allocator, argsv);
+    try server.use_zerver();
+    try server.start();
+    _ = gpa.deinit();
 }
-
-test "simple test" {}
